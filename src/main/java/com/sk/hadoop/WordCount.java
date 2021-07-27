32b9fec9abdb6cef60949405b68f1aa4e9ba2cf7
Hoesung Jung <jrps2212@gmail.com>
2021-07-27 11:08:16 +0900

    Initial commit

---
+++ src/main/java/com/sk/hadoop/WordCount.java
@@ -0,0 +1,46 @@
+package com.sk.hadoop;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordCount(), args);
+        System.exit(exitCode);
+    }
+
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output>\n",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = new org.apache.hadoop.mapreduce.Job();
+        job.setJarByClass(WordCount.class);
+        job.setJobName("WordCounter");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        int returnValue = job.waitForCompletion(true) ? 0:1;
+        System.out.println("job.isSuccessful " + job.isSuccessful());
+        return returnValue;
+    }
+}
---
+++ src/main/java/com/sk/hadoop/WordCountMapper.java
@@ -0,0 +1,26 @@
+package com.sk.hadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    @Override
+    protected void map(LongWritable key, Text value,
+                       Context context)
+            throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer st = new StringTokenizer(line," ");
+        while(st.hasMoreTokens()){
+            word.set(st.nextToken());
+            context.write(word,one);
+        }
+    }
+}
